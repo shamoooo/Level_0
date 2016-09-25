c9f7d6d1cf91490c039ce9d346eb6a4e11f97d8a
Natalie Petersen <league@WTS13.attlocal.net>
2016-09-25 16:02:39 -0700

    BBB

---
+++ src/Zodiac.java
@@ -0,0 +1,46 @@
+import javax.swing.JOptionPane;
+
+public class Zodiac {
+public static void main(String[] args) {
+String zodiac=JOptionPane.showInputDialog("What is your zodiac sign?");
+if (zodiac.equals("aquarius")) {
+JOptionPane.showMessageDialog(null, "Then, you are progressive, original, independent, and humanitarian!");
+}
+
+if (zodiac.equals("pisces")){
+JOptionPane.showMessageDialog(null, "Then, you are compassionate, artistic, intuitive, gentle, wise, and musical!");
+}
+if (zodiac.equals("aries")){
+JOptionPane.showMessageDialog(null, "Then, you are courageous, determined, confident, enthusiastic, optimistic, honest, and passionate!");
+}
+if (zodiac.equals("taurus")){
+JOptionPane.showMessageDialog(null, "Then, you are reliable, patient, practical, devoted, responsible, and stable!");
+}
+if (zodiac.equals("gemini")){
+JOptionPane.showMessageDialog(null, "Then, you are gentle, affectionate, curious, and adaptable. You are also able to learn quickly and exchange ideas!");
+}
+if (zodiac.equals("cancer")){
+JOptionPane.showMessageDialog(null, "Then, you are tenacious, highly imaginative, loyal, emotional, sympathetic, and persuasive!");
+}
+if (zodiac.equals("leo")){
+JOptionPane.showMessageDialog(null, "Then, you are creative, passionate, generous, warm-hearted, cheerful, and humorous!");
+}
+if (zodiac.equals("virgo")){
+JOptionPane.showMessageDialog(null, "Then, you are loyal, analytical, kind, hardworking, and practical!");
+}
+if (zodiac.equals("libra")){
+JOptionPane.showMessageDialog(null, "Then, you are coooperative, diplomatic, gracious, fair-minded, and social!");
+}
+if (zodiac.equals("scorpio")){
+JOptionPane.showMessageDialog(null, "Then, you are resourceful, brave, passionate, stubborn, and a true friend!");
+}
+if (zodiac.equals("sagittarius")){
+JOptionPane.showMessageDialog(null, "Then, you are generous, idealistic, and have a great sense of humor!");
+}
+if (zodiac.equals("capricorn")){
+JOptionPane.showMessageDialog(null, "Then, you are responsible, disciplined, display self-control, and you would make a good manager!");
+}
+}
+}
+
+
